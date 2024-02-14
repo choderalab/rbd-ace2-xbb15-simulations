0f68ef9a8e79481cf6bea4e2f77a9ca514e0f7f7
zhang-ivy <ivy.zhang@wustl.edu>
2024-02-14 12:16:23 -0500

    add subdir 1

---
+++ 03_data/1/run_equil_complex.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Set walltime limit
+#BSUB -W 168:00 
+#
+# Set output file
+#BSUB -o run_equil_complex.out
+#
+# Set error file
+#BSUB -eo run_equil_complex.stderr
+#
+# Specify node group
+#BSUB -m "lj-gpu ll-gpu ln-gpu"
+#BSUB -q gpuqueue
+#
+# nodes: number of nodes and GPU request
+#BSUB -n 1 -R "rusage[mem=10]" -sp 25
+#BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
+#
+# job name (default = name of script file)
+#BSUB -J "equil.1.com"
+
+outdir=/data/chodera/zhangi/vir/v29/data/1/
+phase=complex
+
+source ~/.bashrc
+conda activate perses-paper7
+
+python /data/chodera/zhangi/vir/v29/run_md/run_equilibration_long.py $outdir $phase --gentle
